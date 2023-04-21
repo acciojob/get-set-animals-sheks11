4b37f508091768048736725f4d24680bed2716c7
sheks11 <110970118+sheks11@users.noreply.github.com>
2023-04-21 19:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
 //complete this code
 class Animal {
-  constructor(species) {
-    this.species = species;
-  }
+	constructor(species){
+		this.species = species;
+	}
+	getSpecies(){
+		return this.species;
+	}
+	makeSound(){
+		console.log("The "+ this.species + " makes a sound.");
+	}
+}
 
-  makeSound() {
-    console.log("The animal makes a sound.");
-  }
+
+
+class Cat extends Animal {
+	Purr(){
+		console.log("The "+ this.species + " purrs.");
+	}
 }
 
 class Dog extends Animal {
-  constructor(name) {
-    super("Dog");
-    this.name = name;
-  }
-
-  makeSound() {
-    console.log("Woof!");
-  }
+	bark(){
+		console.log("The "+this.species + " barks.");
+	}
 }
-
-class Cat extends Animal {
-  constructor(name) {
-    super("Cat");
-    this.name = name;
-  }
-
-  makeSound() {
-    console.log("Meow!");
-  }
-}
-
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
